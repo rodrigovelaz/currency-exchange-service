cbd743fde3cb3c846f07e9e0448f02d68ea474fa
rodrigovelaz <rodrigo.velaz@gmail.com>
2017-09-12 07:05:26 +0000

    Test

---
+++ src/test/java/org/rodrigovelaz/currencyexchangeservice/CurrencyExchangeServiceApplicationTest.java
@@ -0,0 +1,16 @@
+package org.rodrigovelaz.currencyexchangeservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes=CurrencyExchangeServiceApplication.class)
+public class CurrencyExchangeServiceApplicationTest {
+
+	@Test
+	public void test() {
+	}
+	
+}
