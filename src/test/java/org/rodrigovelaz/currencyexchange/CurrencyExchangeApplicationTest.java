933b0f71f994e3369ea357f6f13ece20cb3cd1aa
Rodrigo Velaz <rvelaz.sow@ten-x.com>
2023-04-22 03:34:05 +0200

    Refactor

--- src/test/java/org/rodrigovelaz/currencyexchange/CurrencyExchangeApplicationTest.java
+++ src/test/java/org/rodrigovelaz/currencyexchange/CurrencyExchangeApplicationTest.java
@@ -1,8 +1,7 @@
-package org.rodrigovelaz.currencyexchangeservice;
+package org.rodrigovelaz.currencyexchange;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.rodrigovelaz.currencyexchange.CurrencyExchangeApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
